cebb3281ef11697813bae07ac53caa24d4f3b9f4
Your Name <you@example.com>
2022-11-13 21:46:11 +0300

    13.11 commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -12,30 +12,30 @@ const character = {
       id: 8,
       name: 'Двойной выстрел',
       icon: 'http://...',
-      description: 'Двойной выстрел наносит двойной урон'
-    }, 
-    {
-      id: 9,
-      name: 'Нокаутирующий удар',
-      icon: 'http://...'
-    }
-  ]	
-}
-
-const output = [
-    {
-      id: 8,
-      name: 'Двойной выстрел',
-      icon: 'http://...',
-      description: 'Двойной выстрел наносит двойной урон'
+      description: 'Двойной выстрел наносит двойной урон',
     },
     {
       id: 9,
       name: 'Нокаутирующий удар',
       icon: 'http://...',
-      description: 'Описание недоступно'
-    }
-  ]
+    },
+  ],
+};
+
+const output = [
+  {
+    id: 8,
+    name: 'Двойной выстрел',
+    icon: 'http://...',
+    description: 'Двойной выстрел наносит двойной урон',
+  },
+  {
+    id: 9,
+    name: 'Нокаутирующий удар',
+    icon: 'http://...',
+    description: 'Описание недоступно',
+  },
+];
 
 describe('testing fuction attackOptions', () => {
   const result = attackOptions(character);
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
 export default function attackOptions(object) {
   const { special } = object;
-  const attackList = [];
-  const resattack = special.map(item => {
-    if (typeof item['description'] === "undefined") {
-      item['description'] = 'Описание недоступно';
+  const attackList = special.map((item) => {
+    if (typeof item['description'] === 'undefined') {
+      item.description = 'Описание недоступно';
     }
     return item;
   });
-  return resattack;
-} 
+  return attackList;
+}
