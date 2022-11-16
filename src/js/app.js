17c48fc9c3f2d94fdf56705fa99816ce9dc8505d
Your Name <you@example.com>
2022-11-17 00:21:16 +0300

    commit 17.11

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 export default function attackOptions(object) {
   const { special } = object;
   const attackList = special.map((item) => {
-    if (typeof item['description'] === 'undefined') {
-      item.description = 'Описание недоступно';
+    if (typeof item.description === 'undefined') {
+      const newItem = { ...item };
+      newItem.description = 'Описание недоступно';
+      return newItem;
     }
     return item;
   });
